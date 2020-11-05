373add5f0a75c6dcc1238e91115049f6648d7259
Hector Meneses <hectormeneses@unicauca.edu.co>
2020-11-04 19:46:31 -0500

    error

---
+++ pruebas1/src/main/java/com/restaurantePro/compra/Pruebas1Application.java
@@ -0,0 +1,13 @@
+package com.restaurantePro.compra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pruebas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pruebas1Application.class, args);
+	}
+
+}
---
+++ pruebas1/src/test/java/com/restaurantePro/compra/Pruebas1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantePro.compra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pruebas1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
