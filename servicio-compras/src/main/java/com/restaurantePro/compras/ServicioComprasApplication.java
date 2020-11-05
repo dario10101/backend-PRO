96a84871183cce0719ca795f13fa5cae42a287eb
rubendorado <rubendorado@unicauca.edu.co>
2020-11-04 19:24:19 -0500

    Creacion servicio compras

---
+++ servicio-compras/src/main/java/com/restaurantePro/compras/ServicioComprasApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantePro.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioComprasApplication.class, args);
+	}
+
+}
---
+++ servicio-compras/src/test/java/com/restaurantePro/compras/ServicioComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantePro.compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
