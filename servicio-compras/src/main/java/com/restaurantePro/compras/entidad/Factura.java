c2324e114b53783e7c91f3bfa55460ca336fb1d8
Hector Meneses <hectormeneses@unicauca.edu.co>
2020-11-05 23:00:42 -0500

    menejo de fechas para la factura

--- servicio-compras/src/main/java/com/restaurantePro/compras/entidad/Factura.java
+++ servicio-compras/src/main/java/com/restaurantePro/compras/entidad/Factura.java
@@ -1,5 +1,7 @@
 package com.restaurantePro.compras.entidad;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -11,7 +13,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -43,6 +48,8 @@ public class Factura {
 	private Long atrIdCliente;
 	@Column(name = "fac_estado")
 	private String atrEstado;
+	@Column(name = "fac_fecha")
+	private LocalDate atrFecha;
 	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 	@OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
 	@JoinColumn(name = "fac_items")
@@ -60,5 +67,14 @@ public class Factura {
 	{
 		return "Factura menu del dia";
 	}
+	
+    @PrePersist
+    public void prePersist() {
+        this.atrFecha = LocalDate.now();
+        System.out.println("\n\n dia del mes "+this.atrFecha.getDayOfMonth());
+        System.out.println("\n\n mes "+this.atrFecha.getMonth().getValue());
+        System.out.println("\n\n anio "+this.atrFecha.getYear());
+       
+    }
 
 }
--- servicio-compras/src/main/resources/data.sql
+++ servicio-compras/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO tbl_facturas(fac_id, fac_descripcion, fac_idcliente, fac_estado) VALUES ( 2 , ' factura menu del dia ' , 1 , ' CREADO ' );
-INSERT INTO tbl_facturas(fac_id, fac_descripcion, fac_idcliente, fac_estado) VALUES ( 3 , ' factura menu del dia ' , 2 , ' CREADO ' );
+INSERT INTO tbl_facturas(fac_id, fac_descripcion, fac_idcliente, fac_fecha, fac_estado) VALUES ( 2 , ' factura menu del dia ' , 1 , NOW(), ' CREADO ' );
+INSERT INTO tbl_facturas(fac_id, fac_descripcion, fac_idcliente, fac_fecha, fac_estado) VALUES ( 3 , ' factura menu del dia ' , 2 , NOW(), ' CREADO ' );
 
 
 INSERT INTO tbl_itemfacturas(item_id, item_cantidad, item_precio, item_producto_id) VALUES ( 1, 1, 178.89, 1 );
